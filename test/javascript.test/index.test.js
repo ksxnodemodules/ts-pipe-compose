98cec4e8e48a598b31f6554d6bb897f6efc39915
khai96_ <hvksmr1996@gmail.com>
2019-08-20 13:53:47 +0700

    Clean up

--- test/javascript.test/index.test.js
+++
@@ -1,5 +0,0 @@
-const text = require('../../lib/javascript')
-
-it('works', () => {
-  expect(text).toBe('Hello, World!!')
-})
--- test/typescript.test/index.test.ts
+++
@@ -1,5 +0,0 @@
-import text from '../../lib/typescript'
-
-it('works', () => {
-  expect(text).toBe('Hello, World!!')
-})
