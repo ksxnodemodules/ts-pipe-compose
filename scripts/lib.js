2859dba15e5ad54c610ff3f1d62c12f97c8ea8b6
khai96_ <hvksmr1996@gmail.com>
2019-09-10 11:40:07 +0700

    Rename command parameter to cmd
    to avoid name collision

--- scripts/lib.js
+++ scripts/lib.js
@@ -2,12 +2,12 @@ const { spawnSync } = require('child_process')
 
 /**
  * Execute a command
- * @param {string} command Command to run
+ * @param {string} cmd Command to run
  * @param  {...string} args Arguments to pass to command
  */
-function command (command, ...args) {
-  console.info('$', command, ...args)
-  const { error, status } = spawnSync(command, args, { stdio: 'inherit' })
+function command (cmd, ...args) {
+  console.info('$', cmd, ...args)
+  const { error, status } = spawnSync(cmd, args, { stdio: 'inherit' })
   if (error) throw error
   if (status) throw process.exit(status)
 }
