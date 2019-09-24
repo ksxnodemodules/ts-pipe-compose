47f9fa70b7cfb1a6322e92932c6e87bcaee60f15
khai96_ <hvksmr1996@gmail.com>
2019-09-24 08:05:33 +0700

    Add codegen build step to 'jest' script

--- scripts/run-jest.js
+++ scripts/run-jest.js
@@ -1,4 +1,5 @@
 'use strict'
 const { command, skippableCommand } = require('./lib')
+skippableCommand('SKIP_CODEGEN', 'npm', 'run', 'codegen')
 skippableCommand('SKIP_COMPILE', 'npm', 'run', 'compile:cjs')
 command('jest', '--coverage')
